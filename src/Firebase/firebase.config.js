e96f7ef7c96974f27d148de2451f36e1126dc7e7
arifulislam38 <mdarifulislam0238@gmail.com>
2022-11-10 19:52:32 +0600

    env variable added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,13 +5,22 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDXI-y1zC9cCNqUR_X7ljBNsW-2bwOOLEg",
-  authDomain: "wedding-photography-aaba3.firebaseapp.com",
-  projectId: "wedding-photography-aaba3",
-  storageBucket: "wedding-photography-aaba3.appspot.com",
-  messagingSenderId: "1067598460008",
-  appId: "1:1067598460008:web:6c5ec0657536c4d28737dd"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDXI-y1zC9cCNqUR_X7ljBNsW-2bwOOLEg",
+//   authDomain: "wedding-photography-aaba3.firebaseapp.com",
+//   projectId: "wedding-photography-aaba3",
+//   storageBucket: "wedding-photography-aaba3.appspot.com",
+//   messagingSenderId: "1067598460008",
+//   appId: "1:1067598460008:web:6c5ec0657536c4d28737dd"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
